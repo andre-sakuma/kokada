9148c7fdb4d9537ff63bfd4121fadb1eb63f62ff
André Sakuma <andre.yuji.sakuma@gmail.com>
2023-06-13 15:10:22 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="left">
+      <button class="submit">Prova obrigatória</button>
+      <button class="submit">Simulado</button>
+      <button class="submit">Upload de questões</button>
+    </div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: var(--white);
+}
+
+.left {
+  min-width: 300px;
+  padding: 0 32px;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.submit {
+  background-color: var(--red);
+  border-radius: 4px;
+  color: var(--white);
+  font-weight: 800;
+}
+
+.input h3 {
+  margin: 0;
+}
+.input input {
+  border: 1px solid var(--white);
+  padding: 0 8px;
+  border-radius: 4px;
+  height: 25px;
+  width: calc(100% - 24px);
+}
+
+.right {
+  flex-grow: 1;
+  width: 100%;
+}
+</style>
---
+++ src/views/Perfil.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h1>Dados pessoais</h1>
+    <p>email:</p>
+    <span>teste@policc.com</span>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 32px;
+  background-color: var(--white);
+  color: var(--blue);
+}
+</style>
